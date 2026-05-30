893dcb721c2de8e4c95f89f2522225400bed45ae
ouss2312-source <ouss23122010@gmail.com>
2026-05-30 10:30:17 +0700

    Codes

--- code/led_reactor.ino
+++ code/led_reactor.ino
@@ -1,3 +1,3 @@
 // Arc Reactor LED glow effect
 // To be written in Session 4 using FastLED library
-// Target: WS2812B 12-bit ring, blue breathing pulse at ~60 BPM
+// Target: WS2812B 16-bit ring, blue breathing pulse at ~60 BPM
